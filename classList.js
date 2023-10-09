cab71f3195a02d6371b4736f9ef70bb8a9208636
LETICIA EVELYN DE SOUZA LUBRIGATE <leticia.eslubrigate@senacsp.edu.br>
2023-10-09 16:21:03 -0300

    first commit

---
+++ classList.js
@@ -0,0 +1,38 @@
+/*function minhaFuncao(){
+    document.getElementById('minhaDiv').classList.add('classe1')
+}
+
+function minhaRemover(){
+    document.getElementById('minhaDiv').classList.remove('classe1','classe2','classe3','classe4')
+} */
+
+/*
+function corAzul(){
+    document.getElementById('minhaDiv').classList.add('classe1')
+/*fundo azul como fazer 
+
+    let elemento = document.getElementById("minhaDiv")
+    elemento.style.backgroundColor = "blue"
+    /*fundo azul como fazer de outro jeito
+}
+
+function fundoTransp(){
+    document.getElementById('minhaDiv').classList.add('classe2')
+/*fundo transparente como fazer
+
+    document.getElementById("minhaDiv").style.backgroundColor = "transparent"
+    /*fundo transparente como fazer de outra forma
+}
+
+function corVermelha(){
+    document.getElementById('minhaDiv').classList.add('classe3')
+    /*fundo vermelho
+}
+
+function corPreta(){
+    document.getElementById('minhaDiv').classList.add('classe4')
+    /*fundo normal
+}
+*/
+
+
